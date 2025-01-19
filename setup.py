f2731c4a0774d0bcf77be949c27ac6d487a6eca6
Daniel <d.kummel@penterep.com>
2025-01-19 20:11:16 +0100

    Bump version to 0.0.9

--- setup.py
+++ setup.py
@@ -30,6 +30,5 @@ setuptools.setup(
         "homepage":   "https://www.penterep.com/",
         "repository": "https://github.com/penterep/ptcookiechecker",
         "tracker":    "https://github.com/penterep/ptcookiechecker/issues",
-        "changelog":  "https://github.com/penterep/ptcookiechecker/blob/main/CHANGELOG.md",
     }
 )
